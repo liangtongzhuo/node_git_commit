fac54237039cefffaa285cdb7bd8419c52eb8be5
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-19 11:43:55 +0800

    first commit!123

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const path = ''; //目录，为空就是当前目录
 require('simple-git')()
      .add('./*')
-     .commit("first commit!")
+     .commit("first commit!123")
      .push(['-u', 'origin', 'master'], (e) => {
         console.log('done',e);
      });
