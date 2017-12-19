cf2e6738d2e79f929466c4a4c48a83f5b630bc4c
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-19 13:10:07 +0800

    自动 commit，时间Tue Dec 19 2017 13:10:07 GMT+0800 (CST)

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ simpleGit()
      .commit('自动 commit，时间' + time)
      .push(['-u', 'origin', 'master'], (e) => {
         console.log('commit 成功，时间：' + time)
+        upDataFile();
      });
 
-fs.appendFile(__dirname + '/README.md', '# 上一次自动 commit，时间:' + time, err => {
-  err ? console.error('缺少 README.md ') : console.log('追加 README 成功，时间: ' + time)
-});
+// 修改 README 文件
+function upDataFile() {
+  fs.appendFile(__dirname + '/README.md', '### 上一次自动 commit，时间:' + time + '\r\n', err => {
+    err ? console.error('缺少 README.md ') : console.log('追加 README 成功，时间: ' + time)
+  });
+}
