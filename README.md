74e181793a89d8454b228ae0561f608309354460
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-19 13:17:41 +0800

    自动 commit，时间Tue Dec 19 2017 13:17:41 GMT+0800 (CST)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Node_git_commit
 使用 Node 操作 GitHub
 ### 上一次自动 commit，时间:Tue Dec 19 2017 13:10:07 GMT+0800 (CST)
+### 上一次自动 commit，时间:Tue Dec 19 2017 13:17:07 GMT+0800 (CST)
--- index.js
+++ index.js
@@ -24,6 +24,6 @@ setInterval(function () {
 // 修改 README 文件
 function upDataFile(time) {
   fs.appendFile(__dirname + '/README.md', '### 上一次自动 commit，时间:' + time + '\r\n', err => {
-    err ? console.error('缺少 README.md ') : console.log('追加 README 成功，时间: ' + time)
+    err ? console.error('缺少 README.md ') : console.log('README 追加成功，时间: ' + time)
   })
 }
