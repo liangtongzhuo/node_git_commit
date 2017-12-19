d777fef7409a73b23dfab302e5667612d70e8aa1
liangtongzhuo@gmail.com <liangtongzhuo@gmail.com>
2017-12-19 11:51:03 +0800

    自动 commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Node_git_commit
 使用 Node 操作 GitHub
+更新成功
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict'
-const path = ''; //目录，为空就是当前目录
-require('simple-git')()
+const path = ''; //.git 目录，为空就是当前目录
+const fs = require('fs')
+require('simple-git')(path)
      .add('./*')
-     .commit("first commit!123")
+     .commit("自动 commit")
      .push(['-u', 'origin', 'master'], (e) => {
-        console.log('done',e);
+        console.log('commit 成功');
      });
+
+fs.appendFile(__dirname + '/README.md', '更新成功', err => {
+  if(err) {
+    console.error(err);
+  } else {
+    console.log('追加成功');
+  }
+});
